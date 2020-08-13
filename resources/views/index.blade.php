d96a700fb76cc628301eb32383b7dd9e62f13579
Johan Cortes <joco1204@gmail.com>
2020-08-12 20:37:35 -0500

    Modificacion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col col-md-6">
-                <p>Hola Mundo</p>
-            </div>
-            <div class="col col-md-6">
-                <p>Hola Mundo</p>
-            </div>
-        </div>
+        
     </div>
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -52,3 +52,28 @@
 
     </head>
     <body>
+
+    <section>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/">Inicio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('trabajadores') }}">Trabajadores <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('ordenes-de-trabajo') }}">Ordenes de Trabajo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Informe</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+    <section>
+        <content>
