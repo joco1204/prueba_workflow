d0892db0879187946577a21f42af0199ec6e9565
Johan Cortes <joco1204@gmail.com>
2020-08-12 20:49:22 -0500

    Modificaciones

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        
+        <div class="row">
+        	<div class="col-md-4 text-center">
+        		<a href="{{ route('trabajadores') }}" class="btn btn-success btn-lg" style="width: 250px;">Trabajadores</a>
+        	</div>
+        	<div class="col-md-4 text-center">
+        		<a href="{{ route('ordenes-de-trabajo') }}" class="btn btn-primary btn-lg" style="width: 250px;">Ordenes de trabajo</a>
+        	</div>
+        	<div class="col-md-4 text-center">
+        		<a href="/" class="btn btn-danger btn-lg" style="width: 250px;">Informe</a>
+        	</div>
+        </div>
     </div>
 @endsection
